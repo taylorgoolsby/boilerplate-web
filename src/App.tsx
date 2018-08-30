d269b3795125f2d795dee161a0cf9a27d9f56032
Taylor Goolsby <taylorgoolsby@users.noreply.github.com>
2018-08-29 23:57:03 -0700

    added styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import { hot } from 'react-hot-loader'
+import styled from 'styled-components'
+
+const StyledApp = styled.div`
+  width: 100vw;
+  min-height: 100vh;
+  background-color: papayawhip;
+  color: palevioletred;
+`
 
 class App extends React.Component {
   public render() {
     return (
-      <div>
+      <StyledApp>
         Hello World!
-      </div>
+      </StyledApp>
     )
   }
 }
